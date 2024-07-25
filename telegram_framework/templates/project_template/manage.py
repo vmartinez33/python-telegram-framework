6977ebf505de15f687f415f854ec6deaa2e77e89
vmartinez33 <108585032+vmartinez33@users.noreply.github.com>
2024-07-25 12:06:39 +0200

    Copied manage.py from django projects

--- telegram_framework/templates/project_template/manage.py
+++ telegram_framework/templates/project_template/manage.py
@@ -1,7 +1,21 @@
-# telegram_framework/templates/project_template/manage.py
+"""Command-line utility for administrative tasks."""
+import os
+import sys
+
 
 def main():
-    print("Manage your project here.")
+    """Run administrative tasks."""
+    # os.environ.setdefault('TELEGRAM_SETTINGS_MODULE', 'settings')
+    try:
+        # from django.core.management import execute_from_command_line
+        pass
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Telegram Framework. Did you "
+            "forget to activate a virtual environment and install all dependencies?"
+        ) from exc
+    # execute_from_command_line(sys.argv)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
