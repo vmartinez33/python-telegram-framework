02952d82bc704ae2fa5ebcaecd5142c17d84164e
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-01 17:46:20 +0200

    createmodule command almost implemented

--- telegram_framework/management/commands/createmodule.py
+++ telegram_framework/management/commands/createmodule.py
@@ -12,3 +12,11 @@ class Command(TemplateCommand):
     
     def __init__(self, argv):
         super().__init__(argv)
+        
+    def handle(self, **options):
+        name = options.get('name')
+        #TODO: cambiar 'destination' para que el modulo se cree dentro de la carpeta 'app' del proyecto actual (necesario obtener path del proyecto actual)
+        #TODO (EXTRA): definir en settings.py la ruta relativa por defecto donde crear los modulos (dentro del proyecto por supuesto)
+        destination = os.path.join(os.getcwd(), name)
+        self.copy_template(destination)
+        print(f"Module '{name}' created successfully at {destination}\n")
