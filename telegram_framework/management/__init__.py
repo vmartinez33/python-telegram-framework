a3d0abdc3aa49047b7d6cf709af78b8b4a641468
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-01 13:00:18 +0200

    Made code of execute_from_command_line

--- telegram_framework/management/__init__.py
+++ telegram_framework/management/__init__.py
@@ -1,10 +1,26 @@
 import sys
+import importlib
 
-from telegram_framework.management.commands import startproject # Esto es temporal
+from telegram_framework.management.base import CommandError
 
 def execute_from_command_line(argv=None):
     argv = argv or sys.argv
     print(argv)
-    
-    if argv[1] == 'startproject':
-        startproject.main()
+
+    if len(argv) < 2:
+        print("Please provide a command.")
+        return
+
+    command_name = argv[1]
+    try:
+        module = importlib.import_module(f'commands.{command_name}')
+        command_instance = module.Command()
+        command_instance.execute(*argv[2:])
+    except ModuleNotFoundError:
+        print(f"Command '{command_name}' could not be found.")
+    except AttributeError:
+        print(f"The module '{command_name}' does not have a 'Command' class.")
+    except CommandError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
