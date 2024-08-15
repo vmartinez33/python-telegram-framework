07f68f890e5dc9e88755a073ecbcb0af604ef04d
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-15 11:00:33 +0200

    Add conf utils

---
+++ telegram_framework/conf/utils.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+def set_environment(path = __file__, project_module = 'app.settings'):
+    os.environ.setdefault('TELEGRAM_SETTINGS_MODULE', project_module)
+    file_dir = os.path.abspath(os.path.dirname(path))
+    if file_dir not in sys.path:
+        sys.path.insert(0, file_dir)
