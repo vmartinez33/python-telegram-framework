dd97157b96f02d8f25d84f1c5f1fe76ac367c6e7
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-15 11:02:13 +0200

    Create Settings class
    Under construction

---
+++ telegram_framework/conf/__init__.py
@@ -0,0 +1,9 @@
+from telegram_framework.core.exceptions import ImproperlyConfigured
+
+ENVIRONMENT_VARIABLE = "TELEGRAM_SETTINGS_MODULE"
+
+class Settings:
+    def __init__(self) -> None:
+        pass
+
+settings = Settings()
