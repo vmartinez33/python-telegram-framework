b4013896449358b5e5895a4f2fe5a5a77209e05c
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-17 12:02:01 +0200

    Webhook settings

--- telegram_framework/conf/default_settings.py
+++ telegram_framework/conf/default_settings.py
@@ -6,6 +6,17 @@ DEBUG = False
 MODULES_DIR = 'app.modules'
 HANDLERS_MODULE = 'app.handlers'
 
+# Webhook configuration
+USE_WEBHOOK = False
+
+WEBHOOK_LISTEN = "127.0.0.1"
+WEBHOOK_PORT = 80
+WEBHOOK_URL = "http://localhost/telegram"
+WEBHOOK_IP_ADDRESS = None
+WEBHOOK_SECRET_TOKEN = None
+WEBHOOK_CERT = None
+WEBHOOK_SSL_KEY = None
+
 # Telegram Bot configuration
 TELEGRAM_BOT_TOKEN = ''
 
--- telegram_framework/templates/project_template/app/settings.py
+++ telegram_framework/templates/project_template/app/settings.py
@@ -7,5 +7,16 @@ For the full list of settings and their values, see:
 # Project configuration
 DEBUG = True
 
+# Webhook configuration
+USE_WEBHOOK = False
+
+WEBHOOK_LISTEN = "127.0.0.1"
+WEBHOOK_PORT = 80
+WEBHOOK_URL = "http://localhost/telegram"
+WEBHOOK_IP_ADDRESS = None
+WEBHOOK_SECRET_TOKEN = None
+WEBHOOK_CERT = None
+WEBHOOK_SSL_KEY = None
+
 # Telegram Bot configuration
 TELEGRAM_BOT_TOKEN = 'MY BOT TOKEN'
