859ebb249e60433579719f640da74266f84f5f0f
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-25 19:02:43 +0200

    Add debug parameter
    If True, exceptions are raised

--- telegram_framework/management/__init__.py
+++ telegram_framework/management/__init__.py
@@ -3,7 +3,7 @@ import importlib
 
 from telegram_framework.management.base import CommandError
 
-def execute_from_command_line(argv=None):
+def execute_from_command_line(argv=None, debug=False):
     argv = argv or sys.argv
 
     if len(argv) < 2:
@@ -15,11 +15,15 @@ def execute_from_command_line(argv=None):
         module = importlib.import_module(f'telegram_framework.management.commands.{command_name}')
         command = module.Command(argv)
         command.execute()
-    except ModuleNotFoundError:
+    except ModuleNotFoundError as e:
         print(f"Command '{command_name}' could not be found.")
-    except AttributeError:
+        if debug: raise e
+    except AttributeError as e:
         print(f"The module '{command_name}' does not have a 'Command' class.")
+        if debug: raise e
     except CommandError as e:
         print(e)
+        if debug: raise e
     except Exception as e:
         print(f"Unexpected error: {e}")
+        if debug: raise e
