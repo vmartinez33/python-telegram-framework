85125c7e52705711d2ad9667ae2a572a52db5944
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-08-25 19:41:43 +0200

    feat (test_initialization): check if some files exists

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -1,3 +1,22 @@
+import os
+
+from telegram_framework.conf import settings
 
 def test_initialized_project(temporary_project):
-    project_dir = temporary_project.project_dir
+    project_dir = temporary_project.get('project_dir', None)
+    assert project_dir is not None
+    
+    # Check if app, manage, settings, handlers and callbacks files are present
+    app_file = os.path.join(project_dir, 'app.py')
+    manage_file = os.path.join(project_dir, 'manage.py')
+    settings_file = os.path.join(project_dir, 'app', 'settings.py')
+    handlers_file = os.path.join(project_dir, 'app', 'handlers.py')
+    callbacks_file = os.path.join(project_dir, 'app', 'callbacks.py')
+    assert (
+        os.path.isfile(app_file) and 
+        os.path.isfile(manage_file) and 
+        os.path.isfile(settings_file) and
+        os.path.isfile(handlers_file) and
+        os.path.isfile(callbacks_file)
+    )
+    
