ab7e5bb9f7a6f36438b750d25301a7b59e280f8a
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-10-23 21:25:41 +0200

    feat: added decorators to define handlers

---
+++ telegram_framework/handlers/decorators.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from telegram.ext import filters, CommandHandler, MessageHandler
+from telegram.ext.filters import BaseFilter
+
+
+def command(command_name: str, filters: Optional[BaseFilter] = None, block: bool = True, has_args: Optional[bool | int] = None) -> CommandHandler:
+    """Decorator to return a CommandHandler for a callback function."""
+    def decorator(func):
+        return CommandHandler(command_name, func, filters, block, has_args)
+    return decorator
+
+
+def message(filters: Optional[BaseFilter] = filters.TEXT & ~filters.COMMAND, block: bool = True) -> CommandHandler:
+    """Decorator to return a CommandHandler for a callback function."""
+    def decorator(func):
+        return MessageHandler(filters, func, block)
+    return decorator
+
