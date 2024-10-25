b4f73449877b548f07701ec080a1a292c10a4105
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-10-25 18:26:56 +0200

    feat (template): changed project template to use new decorators

--- telegram_framework/templates/project_template/app.py
+++ telegram_framework/templates/project_template/app.py
@@ -6,27 +6,27 @@ from telegram_framework.core.app import BotApp
 
 app = BotApp(__name__)
 
-# @command('start')
-# async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Send a message when the command /start is issued."""
-#     user = update.effective_user
-#     await update.message.reply_html(f"Hi {user.name}!")
 
+@app.command('start')
+async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(f"Hi {user.name}!")
 
-# @command('help')
-# async def cmd_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Send a message when the command /help is issued."""
-#     await update.message.reply_html(
-#         f"These are the available commands:\n \
-#         &#8226; \"<b>/start</b>\"\n \
-#         &#8226; \"<b>/help</b>\""
-#     )
+@app.command('help')
+async def cmd_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_html(
+        f"These are the available commands:\n \
+        &#8226; \"<b>/start</b>\"\n \
+        &#8226; \"<b>/help</b>\""
+    )
 
+@app.message()
+async def msg_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
 
-# @message()
-# async def msg_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Echo the user message."""
-#     await update.message.reply_text(update.message.text)
 
 if __name__ == "__main__":
     app.run()
